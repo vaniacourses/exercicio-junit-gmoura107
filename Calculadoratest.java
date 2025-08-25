168e57d05caf12fc902311655f23cd6541b6fc92
gmoura107 <54088749+gmoura107@users.noreply.github.com>
2025-08-25 20:00:15 -0300

    Create Calculadoratest.java

---
+++ Calculadoratest.java
@@ -0,0 +1,14 @@
+package Calculadora;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+	@Test
+	public void somaTest() {
+		Calculadora calc = new Calculadora();
+		int resultado = calc.soma(5, 3);
+		assertEquals(8,resultado);
+	}
+}
